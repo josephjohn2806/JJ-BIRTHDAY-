fc8eb679d33457924c18460a8542dbaf8342ecc5
josephjohn2806 <josephsjohn2806@gmail.com>
2026-06-10 18:14:25 +0300

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+tfrom flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -13,3 +13,20 @@ def process_exploration_request():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/explore', methods=['POST'])
+def process_main_explore():
+    return jsonify({"status": "success", "msg": "Navigating from Main landing page"})
+
+@app.route('/package-inquiry', methods=['POST'])
+def process_package_inquiry():
+    print("Inquiry form entry received from package tracking page.")
+    return jsonify({"status": "success", "message": "Inquiry successfully recorded!"})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
